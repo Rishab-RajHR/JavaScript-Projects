3954231ea2ba7f4ee395cc6ec172b01bf24f3112
Rishab Raj <rr0948555@gmail.com>
2026-05-24 18:49:03 +0530

    Mini

--- MiniCalendar/script.js
+++ MiniCalendar/script.js
@@ -4,7 +4,9 @@ const month = document.getElementById("month");
 const year = document.getElementById("year");
 
 const today = new Date();
-console.log(today);
+
+const weekDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const allMonths = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
 date.innerHTML = today.getDate();
 day.innerHTML = today.getDay();
