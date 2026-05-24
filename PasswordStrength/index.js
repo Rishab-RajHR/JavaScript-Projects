9142c714e532897b0f92fefe53518f1e17168f9a
Rishab Raj <rr0948555@gmail.com>
2026-05-24 19:30:49 +0530

    password

--- PasswordStrength/index.js
+++ PasswordStrength/index.js
@@ -11,11 +11,17 @@ pass.addEventListener('input', () => {
      }
      if(pass.value.length < 4){
         str.innerHTML = "weak";
+        pass.style.borderColor = "#ff5925";
+        msg.style.color = "#ff5925";
      }else if(pass.value.length >= 4 && pass.value.length < 8){
         str.innerHTML = "medium";
+        pass.style.borderColor = "#97891d";
+        msg.style.color = "#97891d";
      }
      else if(pass.value.length >= 8){
         str.innerHTML = "strong";
+        pass.style.borderColor = "#26d730";
+        msg.style.color = "#26d730";
      }
 
 })
